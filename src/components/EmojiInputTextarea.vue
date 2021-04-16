1109275d364b1e85a213168ca30b8c62d299456e
Ferdous Anam <ferdous.anam@gmail.com>
2021-04-16 14:53:11 +0600

    Demo

---
+++ src/components/EmojiInputTextarea.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="form-group">
+    <label for="MeteorEmojiInputTextarea">Input Textarea</label>
+    <textarea class="form-control" id="MeteorEmojiInputTextarea" data-meteor-emoji="true"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MeteorEmojiInputTextarea"
+}
+</script>
+
+<style scoped>
+</style>
